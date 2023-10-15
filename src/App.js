3b3767392d15ae71e9bedee101f78468a2e7447a
Manisha Gupta <guptamanisha8835@gmail.com>
2023-10-15 17:39:46 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+// import Navbar from './component/Navbar/Navbar';
+import Home from './component/Home/Home';
 import './App.css';
+// import Services from './component/Services/Services';
+import Store from './component/Store/Store';
+// import Login from './component/Login/login'
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+// import { Switch } from 'react-router-dom';
+// import Navbar from './component/Navbar/Navbar';
+// import Card from './component/Cards/cards';
+import Gallery from './component/Gallery/gallery';
+import Contact from './component/contact/Contact';
+import AboutUs from '../src/component/About/About';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    
+    <BrowserRouter>
+     
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/store" element={<Store/>}/>
+      <Route path='/AboutUs' element={<AboutUs/>}/>
+      <Route path='/gallery' element={<Gallery/>}/>
+      <Route path='/ContactUs' element={<Contact/>}/>
+   
+      </Routes>
+      
+      </BrowserRouter>
+      </div>
   );
 }
 
